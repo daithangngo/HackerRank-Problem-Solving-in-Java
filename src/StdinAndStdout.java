f9cca8379e585451e4e71b0a0190543111db2260
daithangkim <88834424+daithangkim@users.noreply.github.com>
2021-08-24 18:40:35 +0200

    Java Stdin and Stdout I & II

--- src/StdinAndStdout.java
+++ src/StdinAndStdout.java
@@ -6,6 +6,7 @@ import java.util.Scanner;
 
 public class StdinAndStdout {
     public static void main(String[] args) {
+        // I
         Scanner scanner = new Scanner(System.in);
 
         int a = scanner.nextInt();
@@ -13,5 +14,16 @@ public class StdinAndStdout {
         int c = scanner.nextInt();
 
         System.out.println(a + "\n" + b + "\n" + c + "\n");
+
+
+        //II
+        Scanner sc = new Scanner(System.in);
+        int i = sc.nextInt();
+        double d = sc.nextDouble();
+        //bewegt diesen scanner über die aktuelle Zeile hinaus und gibt die übersprungene Eingabe zurück
+        sc.nextLine();
+        String s = sc.nextLine();
+
+        System.out.println("String: " + s + "\n" + "Double: " + d + "\n" + "Int: " + i);
     }
 }
