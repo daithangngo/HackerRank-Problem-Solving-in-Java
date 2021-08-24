c03e012c44a1c47d3ec77f8fb247981c744cd49e
daithangkim <88834424+daithangkim@users.noreply.github.com>
2021-08-24 17:54:12 +0200

    Java Stdin and Stdout I

--- src/StdinAndStdout.java
+++ src/StdinAndStdout.java
@@ -1,3 +1,19 @@
-public class StdinAndStdout {
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.InputStreamReader;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
 
+public class StdinAndStdout {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
 }
