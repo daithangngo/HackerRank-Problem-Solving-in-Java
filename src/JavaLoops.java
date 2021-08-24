274d78b7c11a4ef66eca1ad81b58f86c19edec33
daithangkim <88834424+daithangkim@users.noreply.github.com>
2021-08-25 01:27:53 +0200

    Java Loops II
    solve single digit input-> invalid one

--- src/JavaLoops.java
+++ src/JavaLoops.java
@@ -1,12 +1,37 @@
 import java.io.IOException;
+import java.math.BigInteger;
 import java.util.Scanner;
 
 public class JavaLoops {
     public static void main(String[] args) throws IOException {
+        /*
+        // I
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         for (int i = 1; i <= 10; i++) {
             System.out.printf("%d x %d = %d\n", n, i, n * i);
         }
+
+         */
+
+        // II
+        Scanner sc = new Scanner(System.in);
+        if(!sc.hasNextInt()){
+            return;
+        }
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int n = sc.nextInt();
+
+        StringBuilder sb = new StringBuilder();
+        int prevNumber = a;
+
+        for (int i = 0; i < n; i++) {
+            prevNumber = prevNumber + (1 << i) * b;
+            sb.append(prevNumber).append(" ");
+
+        }
+
+        System.out.println(sb.toString());
     }
 }
