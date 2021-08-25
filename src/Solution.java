e50c4e862b1be4d2362d7f0fd60fa9b482075c2c
daithangkim <88834424+daithangkim@users.noreply.github.com>
2021-08-25 16:15:52 +0200

    Java Date and Time
    not compiled yet

--- src/Solution.java
+++ src/Solution.java
@@ -1,12 +1,30 @@
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
 import java.util.Scanner;
 
 public class Solution {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String s;
-        if(scanner.hasNextInt()){
-            s = "" + scanner.nextInt();
-            System.out.println("Good job");
-        }
+        String date = scanner.nextLine().trim();
+        int month = Integer.parseInt(date.substring(0, 2));
+        int day = Integer.parseInt(date.substring(2, 4));
+        int year = Integer.parseInt(date.substring(4));
+
+        System.out.println(getDay(month, day, year));
+    }
+
+    static String getDay(int month, int day, int year) {
+        Calendar calendar = new GregorianCalendar(year, month, day);
+        return switch (calendar.get(Calendar.DAY_OF_WEEK)) {
+            case 1 -> "MONDAY";
+            case 2 -> "TUESDAY";
+            case 3 -> "WEDNESDAY";
+            case 4 -> "THURSDAY";
+            case 5 -> "FRIDAY";
+            case 6 -> "SATURDAY";
+            case 7 -> "SUNDAY";
+            default -> throw new IllegalStateException("Unexpected value: " + calendar.get(Calendar.DAY_OF_WEEK));
+        };
     }
 }
