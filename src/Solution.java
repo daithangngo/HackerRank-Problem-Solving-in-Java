70036599c68f17bdb43ba5fe03952e380efdfa10
daithangkim <88834424+daithangkim@users.noreply.github.com>
2021-08-25 15:27:47 +0200

    Java Int to String

---
+++ src/Solution.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String s;
+        if(scanner.hasNextInt()){
+            s = "" + scanner.nextInt();
+            System.out.println("Good job");
+        }
+    }
+}
