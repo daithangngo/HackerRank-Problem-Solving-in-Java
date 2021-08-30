17b52d5a0ab5653003a08cf03d9ec91448794764
daithangkim <88834424+daithangkim@users.noreply.github.com>
2021-08-31 00:26:22 +0200

    Java BigInteger
    working with very big numbers

---
+++ src/Basics/BigIntegers.java
@@ -0,0 +1,20 @@
+package Basics;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class BigIntegers {
+
+    //primeChecker: //System.out.println(sc.nextBigInteger().isProbablePrime(1)? "prime": "not prime");
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner((System.in));
+
+        BigInteger b1 = sc.nextBigInteger();
+        BigInteger b2 = sc.nextBigInteger();
+
+        System.out.println(b1.add(b2));
+        System.out.println(b1.multiply(b2));
+    }
+}
+
