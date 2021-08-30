ae223835523b6488f6e98b150c47bd06be5ed827
daithangkim <88834424+daithangkim@users.noreply.github.com>
2021-08-31 01:05:26 +0200

    Java Exception Handling

--- src/Basics/ExceptionHandling.java
+++ src/Basics/ExceptionHandling.java
@@ -1,5 +1,20 @@
 package Basics;
 
-public class ExceptionHandling {
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
+public class ExceptionHandling {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        try {
+            int x = scanner.nextInt();
+            int y = scanner.nextInt();
+            System.out.println(x / y);
+
+        } catch (InputMismatchException | ArithmeticException e) {
+            System.out.println(e instanceof InputMismatchException
+                    ? "java.util.InputMismatchException":"java.lang.ArithmeticException: / by zero");
+        }
+    }
 }
