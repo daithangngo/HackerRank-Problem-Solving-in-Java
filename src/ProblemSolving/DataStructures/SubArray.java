261680f7d9f2f4edb44a85da37ce45dd188a6011
daithangkim <88834424+daithangkim@users.noreply.github.com>
2021-09-01 00:07:01 +0200

    Java Subarray
    wrong output

---
+++ src/ProblemSolving/DataStructures/SubArray.java
@@ -0,0 +1,25 @@
+package ProblemSolving.DataStructures;
+
+import java.util.Scanner;
+
+public class SubArray {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[] numArr = new int[scanner.nextInt()];
+        for (int i = 0; i < numArr.length; i++) {
+            numArr[i] = scanner.nextInt();
+        }
+
+        int negSubArrCounter = 0;
+        for (int i = 0; i < numArr.length; i++) {
+            int j = i;
+            int sum = 0;
+            while (j != numArr.length - 1) {
+                sum += numArr[j];
+                if (sum < 0) negSubArrCounter++;
+                j++;
+            }
+        }
+        System.out.println(negSubArrCounter);>
+    }
+}
