22a70e7ef7ccad970bd09eca6b5a8e39728028fd
daithangkim <88834424+daithangkim@users.noreply.github.com>
2021-09-04 00:08:24 +0200

    Plus Minus

--- src/Solution.java
+++ src/Solution.java
@@ -17,18 +17,36 @@ public class Solution {
     //Diagonal Difference
     public static int diagonalDifference(List<List<Integer>> arr) {
         int i = 0;
-        int j = arr.size()-1;
-        int primaryDiag =0;
+        int j = arr.size() - 1;
+        int primaryDiag = 0;
         int secondaryDiag = 0;
 
-        while (i != arr.size()){
+        while (i != arr.size()) {
             primaryDiag += arr.get(i).get(i);
             secondaryDiag += arr.get(j).get(i);
             i++;
             j--;
         }
+        return Math.abs(primaryDiag - secondaryDiag);
+    }
 
-        return Math.abs(primaryDiag-secondaryDiag);
+    //Plus Minus
+    public static void plusMinus(List<Integer> arr) {
+        int pos, zero, neg;
+        pos = zero = neg = 0;
+
+        for (int i = 0; i < arr.size(); i++) {
+            if (arr.get(i) > 0) {
+                pos++;
+            } else if (arr.get(i) < 0) {
+                neg++;
+            } else {
+                zero++;
+            }
+        }
+        System.out.printf("%.6f\n", (double) pos / (double) arr.size());
+        System.out.printf("%.6f\n", (double) neg / (double) arr.size());
+        System.out.printf("%.6f\n", (double) zero / (double) arr.size());
     }
 
 
