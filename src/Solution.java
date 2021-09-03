ea564fa1ce1b9a92c007575fb82a232c07755ea2
daithangkim <88834424+daithangkim@users.noreply.github.com>
2021-09-03 23:21:54 +0200

    Diagonal Difference

--- src/Solution.java
+++ src/Solution.java
@@ -7,12 +7,29 @@ public class Solution {
 
     //A Very Big Sum
     public static long aVeryBigSum(List<Long> ar) {
-        // Write your code here
         Iterator<Long> it = ar.iterator();
         long sum = 0;
-        while(it.hasNext())
+        while (it.hasNext())
             sum += it.next();
         return sum;
     }
+
+    //Diagonal Difference
+    public static int diagonalDifference(List<List<Integer>> arr) {
+        int i = 0;
+        int j = arr.size()-1;
+        int primaryDiag =0;
+        int secondaryDiag = 0;
+
+        while (i != arr.size()){
+            primaryDiag += arr.get(i).get(i);
+            secondaryDiag += arr.get(j).get(i);
+            i++;
+            j--;
+        }
+
+        return Math.abs(primaryDiag-secondaryDiag);
+    }
+
 
 }
